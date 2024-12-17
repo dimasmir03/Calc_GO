dad023327d1636e6bb3da72a6d8216de6532e76a
dimasmir03 <saenkodmitriiol@gmail.com>
2024-12-17 22:25:43 +0300

    add application calc

---
+++ cmd/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "github.com/dimasmir03/yandex-licey-go/1.13/internal/application"
+
+func main() {
+	app := application.New()
+	app.Run()
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dimasmir03/yandex-licey-go/1.13
+
+go 1.23.4
